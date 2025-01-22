4ed51eb58418dca135997251cae2c0224d12b3eb
CarlosHP03 <charlie03hp@gmail.com>
2025-01-22 15:36:17 +0000

    Finished test_users

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from app.config import settings
+from app.database import get_db, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pytest
+
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture(scope="function")
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally: db.close()
+
+@pytest.fixture(scope="function")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally: session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "car@gmail.com",
+                 "password": "123"}
+    
+    res = client.post("/users/", json=user_data)
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+
+    assert res.status_code == 201
+
+    return new_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,39 +1,39 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
 from app.config import settings
-from app.database import get_db, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import pytest
+from jose import jwt
 
+# def test_root(client):
+#     res = client.get("/")
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}_test'
+#     assert res.status_code == 200
+#     assert res.json().get('message') == 'Welcome to my API'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally: db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(app)
-
-def test_root():
-    res = client.get("/")
-
-    assert res.status_code == 200
-    assert res.json().get('message') == 'Welcome to my API'
-
-def test_create_user():
-    res = client.post("/users", json={"email": "car@gmail.com", "password": "123"})
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "car@gmail.com", "password": "123"})
     new_user = schemas.UserOut(**res.json())
+
     assert res.status_code == 201
     assert new_user.email == "car@gmail.com"
+
+def test_login_user(client, test_user):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    id = payload.get("user_id")
+
+    assert res.status_code == 200
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+
+@pytest.mark.parametrize("username, password, status_code", [
+                         ('wrongemail@mail.com', '123', 403),
+                         ('car@gmail.com', 'wrongpassword', 403),
+                         ('wrongemail@gmail.com', 'wrongpassword', 403),
+                         (None, '123', 403),
+                         ('car@gmail.com', None, 403)
+                         ])
+def test_failed_login(test_user, client, username, password, status_code):
+    res = client.post("/login", data={"username": username, "password": password})
+
+    assert res.status_code == status_code
