78dd31d50a11acffe87a430624b4701b5f6cda11
CarlosHP03 <charlie03hp@gmail.com>
2025-01-22 12:44:44 +0000

    Added test db

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,25 +2,27 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app import schemas
 from app.config import settings
+from app.database import get_db, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = SessionLocal()
+def override_get_db():
+    db = TestingSessionLocal()
     try:
         yield db
     finally: db.close()
 
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
