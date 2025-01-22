1f58cf2344cb66e34a0dd8691737d1b3244c0f57
CarlosHP03 <charlie03hp@gmail.com>
2025-01-22 15:44:13 +0000

    Added test for posts

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app.config import settings
 from app.database import get_db, Base
+from app.oauth2 import create_access_token
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import pytest
@@ -46,3 +47,16 @@ def test_user(client):
     assert res.status_code == 201
 
     return new_user
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
